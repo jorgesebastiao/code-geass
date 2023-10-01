e32af4950f2ea0230ae2d4d485a870a6c9d3c1d8
jorgesebastiao <jorgenes1@hotmail.com>
2023-10-01 18:39:09 -0300

    Criado estrutura de testes para o projeto CodeGeass.KnightmareFrames.Infra.Data.Tests Criado estrutura de testes para o projeto CodeGeass.Characters.Infra.Data.Tests Criado testes de unidades para o repositorio CharacterRepository

---
+++ projects/server/tests/CodeGeass.Infra.Tests.Common/Base/InfraBaseTest.cs
@@ -0,0 +1,46 @@
+﻿using CodeGeass.Core.Outbox.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGeass.Infra.Tests.Common.Base
+{
+    public abstract class InfraBaseTest
+    {
+        private readonly Mock<IMediator> _mediatorFake;
+        private readonly Mock<IIntegrationEventMapper> __ntegrationEventMapperFake;
+        public DbConnection Connection { get; set; }
+
+        public InfraBaseTest()
+        {
+            _mediatorFake = new Mock<IMediator>();
+            __ntegrationEventMapperFake = new Mock<IIntegrationEventMapper>();
+        }
+
+        protected TContext CreateContext<TContext>()where TContext DbContext
+        {
+            var serviceProvider = new ServiceCollection()
+                         .AddEntityFrameworkSqlite()
+                         .BuildServiceProvider();
+
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+
+            DbContextOptions<TContext> dbContextOptions = new DbContextOptionsBuilder<TContext>()
+                     .UseSqlite(Connection)
+                     .UseApplicationServiceProvider(serviceProvider)
+            .Options;
+
+            var energisaInvoiceBdContext = new EnergisaInvoiceBdContext(dbContextOptions, _mediatorFake.Object, __ntegrationEventMapperFake.Object);
+            energisaInvoiceBdContext.Database.EnsureCreated();
+            return energisaInvoiceBdContext;
+        }
+    }
+}
---
+++ projects/server/tests/CodeGeass.KnightmareFrames.Infra.Data.Tests/Base/InfraBaseTest.cs
@@ -0,0 +1,43 @@
+﻿using CodeGeass.Core.Outbox.Services;
+using CodeGeass.KnightmareFrames.Infra.Data.Contexts;
+using MediatR;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Data.Common;
+
+namespace CodeGeass.KnightmareFrames.Infra.Data.Tests.Base
+{
+    public abstract class InfraBaseTest
+    {
+        private readonly Mock<IMediator> _mediatorFake;
+        private readonly Mock<IIntegrationEventMapper> __ntegrationEventMapperFake;
+        public DbConnection Connection { get; set; }
+
+        public InfraBaseTest()
+        {
+            _mediatorFake = new Mock<IMediator>();
+            __ntegrationEventMapperFake = new Mock<IIntegrationEventMapper>();
+        }
+
+        protected CodeGeassKnightmareFrameBdContext CreateContext()
+        {
+            var serviceProvider = new ServiceCollection()
+                         .AddEntityFrameworkSqlite()
+                         .BuildServiceProvider();
+
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+
+            DbContextOptions<CodeGeassKnightmareFrameBdContext> dbContextOptions = new DbContextOptionsBuilder<CodeGeassKnightmareFrameBdContext>()
+                     .UseSqlite(Connection)
+                     .UseApplicationServiceProvider(serviceProvider)
+            .Options;
+
+            var codeGeassInvoiceBdContext = new CodeGeassKnightmareFrameBdContext(dbContextOptions, _mediatorFake.Object, __ntegrationEventMapperFake.Object);
+            codeGeassInvoiceBdContext.Database.EnsureCreated();
+            return codeGeassInvoiceBdContext;
+        }
+    }
+}
