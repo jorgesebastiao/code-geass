03871ece608089efa15ecab3648114fab1b8ce80
jorgesebastiao <jorgenes1@hotmail.com>
2023-12-02 15:33:47 -0300

    Implementado teste de unidade para o método GetAll do Repositorio KnightmareFrameRepository

---
+++ projects/server/tests/CodeGeass.KnightmareFrames.Infra.Data.Tests/Features/KnightmareFrames/KnightmareFrameRepositoryFixture.cs
@@ -0,0 +1,56 @@
+﻿using CodeGeass.KnightmareFrames.Infra.Data.Features.KnightmareFrames;
+using FluentAssertions;
+using Gwtdo.Scenarios;
+using Gwtdo.Steps;
+
+namespace CodeGeass.KnightmareFrames.Infra.Data.Tests.Features.KnightmareFrames
+{
+    using act = Act<KnightmareFrameRepositoryContext>;
+    using arrange = Arrange<KnightmareFrameRepositoryContext>;
+    using assert = Assert<KnightmareFrameRepositoryContext>;
+
+    public class KnightmareFrameRepositoryFixture : ScenarioFixture<KnightmareFrameRepositoryContext>
+    {
+    }
+
+    public static class Setup
+    {
+        public static arrange I_have_a_repository(this arrange fixtures)
+        {
+            return fixtures.Setup((f) =>
+            {
+                f.KnightmareFrameRepository = new KnightmareFrameRepository(f.Repository, f.DbContext);
+            });
+        }
+
+        public static arrange Add_seed_KnightmareFrames(this arrange fixtures)
+        {
+            return fixtures.Setup((f) =>
+            {
+                f.DbContext.KnightmareFrames.AddRange(KnightmareFrameRepositoryMock.GetKnightmareFrames(10));
+                f.DbContext.SaveChanges();
+            });
+        }
+    }
+
+    public static class Exercise
+    {
+        public static act I_ask_to_getall(this act fixtures)
+        {
+            return fixtures.It(async f => f.Result = await f.KnightmareFrameRepository.GetAllAsync());
+        }
+    }
+
+    public static class Verify
+    {
+        public static assert I_should_have_a_all_KnightmareFrames(this assert fixtures)
+        {
+            return fixtures.Expect(x =>
+            {
+                x.Result.IsSuccess.Should().BeTrue();
+                x.Result.Success.Should().NotBeNull();
+                x.Result.Success.Should().HaveCount(10);
+            });
+        }
+    }
+}
---
+++ projects/server/tests/CodeGeass.KnightmareFrames.Infra.Data.Tests/Features/KnightmareFrames/KnightmareFrameRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using Gwtdo;
+using Xunit;
+
+namespace CodeGeass.KnightmareFrames.Infra.Data.Tests.Features.KnightmareFrames
+{
+    public class KnightmareFrameRepositoryTest : Feature<KnightmareFrameRepositoryContext, KnightmareFrameRepositoryFixture>, IClassFixture<KnightmareFrameRepositoryContext>, IDisposable
+    {
+        public KnightmareFrameRepositoryTest(KnightmareFrameRepositoryContext context) : base(context)
+        {
+        }
+
+        [Fact]
+        public void Should_return_list_of_all_the_KnightmareFrames()
+        {
+            FeatureContext.Setup();
+
+            GIVEN
+                .I_have_a_repository()
+                .Add_seed_KnightmareFrames();
+
+            WHEN
+                .I_ask_to_getall();
+
+            THEN
+                .I_should_have_a_all_KnightmareFrames();
+        }
+
+        public void Dispose()
+        {
+            FeatureContext.TearDown();
+        }
+    }
+}
